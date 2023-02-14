5c54a5ea03c20d1f764e35bc60e4b992f7fc418d
Mason Wooley <mason.wooley@gmail.com>
2023-02-14 10:10:43 -0500

    Add UI elements

---
+++ Assets/UIController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIController : MonoBehaviour
+{
+    public Button attackButton01;
+    public Button attackButton02;
+    public Button attackButton03;
+
+    void Start() {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        attackButton01 = root.Q<Button>("AttackButton01");
+        attackButton02 = root.Q<Button>("AttackButton02");
+        attackButton03 = root.Q<Button>("AttackButton03");
+
+        // Attach the events
+        attackButton01.clicked += attack1Pressed;
+        attackButton02.clicked += attack2Pressed;
+        attackButton03.clicked += attack3Pressed;
+    }
+
+    void attack1Pressed () {
+        Debug.Log("I punch!");
+    }
+
+    void attack2Pressed () {
+        Debug.Log("I kick!");
+    }
+    void attack3Pressed () {
+        Debug.Log("I have a fucking gun!");
+    }
+}
